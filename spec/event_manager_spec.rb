0b2707fec788a833cc712f12a3a00d1d5ebf3d0a
Markus Olsen <molsen13@gmail.com>
2015-01-15 13:52:06 -0700

    Initial commit

---
+++ spec/event_manager_spec.rb
@@ -0,0 +1,23 @@
+require './lib/event_manager'
+
+describe EventManager do
+  attr_reader :event
+  before do
+    @event = EventManager.new
+  end
+
+  it "exists" do
+    expect(event).to be_truthy
+  end
+
+  it "opens CSV file" do
+    #expect(event[0][0]).to include("Allison")
+  end
+
+  describe "#clean_zipcode" do
+    it "pads too smalls with zeros" do
+      expect(event.clean_zipcode("123")).to eq("00123")
+    end
+  end
+
+end
