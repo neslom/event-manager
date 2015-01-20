a3822dfd652231b0da4d502967e79e18a0de9918
Markus Olsen <molsen13@gmail.com>
2015-01-19 21:41:59 -0700

    Decide not to test retroactively

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,93 +4,93 @@ require 'erb'
 require 'pry'
 
 class EventManager
-#Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-#def clean_zipcode(zipcode)
-  #zipcode.to_s.rjust(5, "0")[0..4]
-#end
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
 
-#def number_formatter(number)
-  #[3, 7].each { |x| number.insert(x, "-") }
-  #number
-#end
+def number_formatter(number)
+  [3, 7].each { |x| number.insert(x, "-") }
+  number
+end
 
-#def clean_phone_numbers(number)
-  #return "-" if number.nil?
-  #number.to_s.gsub!(/\D/, "")
-  #if number.length == 10
-    #number_formatter(number)
-  #elsif number.length == 11 && number[0] == "1"
-    #number[0] = ""
-    #number_formatter(number)
-  #else
-    #"-"
-  #end
-#end
+def clean_phone_numbers(number)
+  return "-" if number.nil?
+  number.to_s.gsub!(/\D/, "")
+  if number.length == 10
+    number_formatter(number)
+  elsif number.length == 11 && number[0] == "1"
+    number[0] = ""
+    number_formatter(number)
+  else
+    "-"
+  end
+end
 
-#@registration_hours = []
+@registration_hours = []
 
-#def format_time(time)
-  #DateTime.strptime(time, "%m/%d/%y %k:%M")
-#end
+def format_time(time)
+  DateTime.strptime(time, "%m/%d/%y %k:%M")
+end
 
-#def group_registration_by_hour
-  #@registration_hours.group_by { |time| time.hour }
-#end
+def group_registration_by_hour
+  @registration_hours.group_by { |time| time.hour }
+end
 
-#def print_registration_by_hour
-  #max_hours = group_registration_by_hour.values.max { |a, b| a.length <=> b.length }.length
-  #group_registration_by_hour.select { |k, v| v.length  >= max_hours }.each do |k, v|
-    #puts v[0].strftime("The busiest registration hour(s): %l%p")
-  #end
-#end
+def print_registration_by_hour
+  max_hours = group_registration_by_hour.values.max { |a, b| a.length <=> b.length }.length
+  group_registration_by_hour.select { |k, v| v.length  >= max_hours }.each do |k, v|
+    puts v[0].strftime("The busiest registration hour(s): %l%p")
+  end
+end
 
-#def group_registration_by_weekday
-  #@registration_hours.group_by { |time| time.wday } 
-#end
+def group_registration_by_weekday
+  @registration_hours.group_by { |time| time.wday } 
+end
 
-#def print_registration_by_weekday
-  #max_days = group_registration_by_weekday.values.max { |a, b| a.length <=> b.length }.length
-  #group_registration_by_weekday.select { |k, v| v.length >= max_days }.each do |k, v|
-    #puts v[0].strftime("The busiest registration day(s): %A")
-  #end
-#end
+def print_registration_by_weekday
+  max_days = group_registration_by_weekday.values.max { |a, b| a.length <=> b.length }.length
+  group_registration_by_weekday.select { |k, v| v.length >= max_days }.each do |k, v|
+    puts v[0].strftime("The busiest registration day(s): %A")
+  end
+end
 
-#def legislators_by_zipcode(zipcode)
-  #Sunlight::Congress::Legislator.by_zipcode(zipcode)
-#end
+def legislators_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-#def save_thank_you_letters(id, form_letter)
-  #Dir.mkdir("output") unless Dir.exists?("output")
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
 
-  #filename = "output/thanks_#{id}.html"
+  filename = "output/thanks_#{id}.html"
 
-  #File.open(filename, 'w') do |file|
-    #file.puts form_letter
-  #end
-#end
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
-#puts "EventManager initialized."
+puts "EventManager initialized."
 
-#contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
-#template_letter = File.read("form_letter.erb")
+template_letter = File.read("form_letter.erb")
 
-#erb_template = ERB.new(template_letter)
+erb_template = ERB.new(template_letter)
 
-#contents.each do |row|
-  #id = row[0]
-  #name = row[:first_name]
-  #zipcode = clean_zipcode(row[:zipcode])
-  #numbers = row[:homephone]
-  #time = row[:regdate]
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  numbers = row[:homephone]
+  time = row[:regdate]
 
-  #@registration_hours << format_time(time)
+  @registration_hours << format_time(time)
 
-  #legislators = legislators_by_zipcode(zipcode)
+  legislators = legislators_by_zipcode(zipcode)
 
-  #form_letter = erb_template.result(binding)
+  form_letter = erb_template.result(binding)
 
-  #save_thank_you_letters(id, form_letter)
-#end	
+  save_thank_you_letters(id, form_letter)
+end	
 end
--- spec/event_manager_spec.rb
+++ spec/event_manager_spec.rb
@@ -9,15 +9,4 @@ describe EventManager do
   it "exists" do
     expect(event).to be_truthy
   end
-
-  #it "opens CSV file" do
-    #expect(event[0][0]).to include("Allison")
-  #end
-
-  #describe "#clean_zipcode" do
-    #it "pads too smalls with zeros" do
-      #expect(event.clean_zipcode("123")).to eq("00123")
-    #end
-  #end
-
 end
